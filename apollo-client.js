5b685c4a9e1f4324f30478dbaa7eabfeb07b359c
seunbayo <seunbayo83@gmail.com>
2022-11-28 21:21:55 +0100

    added changes

---
+++ apollo-client.js
@@ -0,0 +1,11 @@
+// ./apollo-client.js
+
+import { ApolloClient, InMemoryCache } from "@apollo/client";
+
+const client = new ApolloClient({
+  uri: "https://api.lens.dev",
+  cache: new InMemoryCache(),
+});
+
+export default client;
+
